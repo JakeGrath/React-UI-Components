95de413e54310fe3d9a8dd4c14cccd9efa131634
JakeGrath <J.A.McGrath@outlook.com>
2019-05-28 06:59:15 -0500

    Finished MVP for Project 1

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+    <h1>Lambda School <a href='#'>@LambdaSchool * 26Jan</a></h1>
+    )
+}
+
+export default HeaderTitle;
